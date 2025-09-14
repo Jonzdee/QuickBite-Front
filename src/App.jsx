07df1f6e59011a2aea0dbc4ab7e12c1df49f41cb
Jonzdee <ogunyankinjohnson@gmail.com>
2025-09-14 15:23:23 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ import Home from "./pages/Customer/Home";
 import Login from "./components/login/Login"
 import SignUp from "./components/login/SignUp";
 import AllowLocation from "./components/login/AllowLocation";
+import Landing from "./pages/landing/Landing";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+     <Landing />
+
         {/* Splash screen */}
         <Route path="/intro" element={<IntroSplash />} />
 
@@ -28,8 +31,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/getlocation" element={<AllowLocation />} />
+   
       </Routes>
     </BrowserRouter>
+
+    
   );
 }
 
---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "../../components/landingComponents/Navbar"
+function Landing() {
+  return (
+    <div >
+        <Navbar />
+    </div>
+  )
+}
+
+export default Landing
